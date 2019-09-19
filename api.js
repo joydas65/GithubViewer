bf70a48e3f01fa5d0c8df3f1ee0bff21c8504da9
Joy Das <35962760+joydas65@users.noreply.github.com>
2019-09-19 17:54:54 +0530

    Create api.js

---
+++ api.js
@@ -0,0 +1,33 @@
+(function() {
+
+    var app = angular.module("githubViewer", []);
+
+    var MainController = function($scope, $http) {
+        var onUserComplete = function(response) {
+            $scope.user = response.data;
+            $http.get($scope.user.repos_url)
+                .then(onRepos, onError);
+        };
+
+        var onRepos = function(response) {
+            $scope.repos = response.data;
+        };
+
+        var onError = function(reason) {
+            $scope.error = "Could not load the data";
+        };
+
+        $scope.search = function(username) {
+            $http.get("https://api.github.com/users/" + username)
+                .then(onUserComplete, onError);
+        };
+
+        $scope.username = "angular";
+
+        $scope.repoSortOrder = "-stargazers_count";
+
+        $scope.message = "Hello welcome to Github Viewer App";
+    };
+
+    app.controller("MainController", ["$scope", "$http", MainController]);
+}());
